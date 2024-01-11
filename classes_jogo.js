cf5529727d6fc1714c1fc036832e674ef9652855
efraimalmeida96 <efraimnba@gmail.com>
2024-01-11 20:11:46 -0300

    classes jogo guerreiro

---
+++ classes_jogo.js
@@ -0,0 +1,61 @@
+/*
+# Escrevendo as classes de um Jogo
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+- Classes e Objetos
+
+## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia
+  guerreiro atacou usando espada */
+
+
+class heroi{
+  constructor(tipo, ataque, arma){
+    this.tipo = tipo
+    this.ataque = ataque
+    this.arma = arma
+  }
+
+  escrever(){
+    console.log(`O ${this.tipo} atacou com ${this.ataque} de poder usando seu/sua ${this.arma}.`)
+  }
+}
+
+let guerreiro = new heroi("Guerreiro", 30, "espada")
+let mago = new heroi("mago", 16, "magia")
+let monge = new heroi("monge", 22, "artes marciais")
+let ninja = new heroi("ninja", 28, "shuriken")
+
+guerreiro.escrever()
+mago.escrever()
+monge.escrever()
+ninja.escrever()
